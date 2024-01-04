690fced1781a88b8835ae97065281fa2687f2890
ak4700159 <ak47001@naver.com>
2024-01-04 20:50:28 +0900

    2024-01-04 1260 clear

--- 1260.cpp
+++ 1260.cpp
@@ -1,11 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <queue>
 
 using namespace std;
-#define MAX_VERTICES 1000
-#define _CRT_SECURE_NO_WARNINGS
 // 정점 번호는 1번부터 N번까지
+// 모든 정점 번호에 대해 
 
 void init_graph(int** g, int n) {
 	for (int i = 0; i < n; i++)
@@ -13,19 +13,13 @@ void init_graph(int** g, int n) {
 			g[i][j] = 0;
 }
 
-
-void insert_edge(int** g, int start, int end, int n) {
-	g[start][end] = 1;
-	g[end][start] = 1;
-}
-
-int bfs_visited[MAX_VERTICES];
+int bfs_visited[10000];
 void bfs(int** g, int n, int start) {
 	queue<int> q;
 
-	q.push(start);
-	printf("%d ", start+1);
-	bfs_visited[start] = 1;
+	q.push(start-1);
+	printf("%d ", start);
+	bfs_visited[start-1] = 1;
 	while (!q.empty()) {
 		int w = q.front();
 		for (int u = 0; u < n; u++) {
@@ -39,7 +33,7 @@ void bfs(int** g, int n, int start) {
 	}
 }
 
-int dfs_visited[MAX_VERTICES];
+int dfs_visited[10000];
 void dfs(int** g, int n, int start) {
 	printf("%d ", start+1);
 	dfs_visited[start] = 1;
@@ -49,41 +43,41 @@ void dfs(int** g, int n, int start) {
 	}
 }
 
-// m = 간선의 개수 n = 정점의 개수 v = 시작 정점
+// n = 정점의 개수 | m = 간선의 개수 | v = 시작 정점
 int main(void) {
-	int m, n, v;
+	int n, m, v;
 	int** adj_map;
 
 	// 입력 
-	do{
-		scanf("%d %d %d", &n, &m, &v);
-		getchar();
-	}while(n < 0 || m < 0 || n > MAX_VERTICES || m > MAX_VERTICES || v < 0 || v > n );
+	scanf("%d %d %d", &n, &m, &v);
+	getchar();
+	if (n < 1 || n > 10000 || m < 1 || m > 10000 || v < 1 || v > n)
+		return 0;
 	// 2차원 배열 할당
 	adj_map = (int**)malloc(sizeof(int*) * n);
 	for (int j = 0; j < n; j++)
 		adj_map[j] = (int*)malloc(sizeof(int) * n);
-	
-	// 기본세팅
+	// 기본세팅 간선 정보 추가 및 그래프 초기화
 	init_graph(adj_map, n);
-	for(int i = 0; i < m; i++) {
-		int start, end;
-		do{
-			scanf("%d %d", &start, &end);
-			getchar();
-		}while(start < 0 || start > n|| end > n || end < 0);
-		start -= 1;
-		end -= 1;
-		insert_edge(adj_map, start, end, n);
+	int accepted_edge = 0;
+	while (accepted_edge != m) {
+		int start = 0, end = 0;
+		scanf("%d %d", &start, &end);
+		getchar();
+		if (start > n || start < 1 || end > n || end < 1) {
+			continue;
+		}
+		adj_map[start - 1][end - 1] = 1;
+		adj_map[end - 1][start - 1] = 1;
+		accepted_edge++;
 	}
 
 	dfs(adj_map, n, v-1);
 	printf("\n");
-	bfs(adj_map, n, v-1);
+	bfs(adj_map, n, v);
 	printf("\n");
 
-
-	for(int w = 0; w < n; w++)
+	for (int w = 0; w < n; w++)
 		free(adj_map[w]);
 	free(adj_map);
 	return 0;
